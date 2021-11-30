c63908833cf17c3bea44e15402b38a90f42aea3d
danielquintao <danielquintaodemoraes@gmail.com>
2021-11-30 19:25:07 +0100

    leapfrog

--- leapfrog.py
+++ leapfrog.py
@@ -1,10 +1,62 @@
+import autograd.numpy as np
+from autograd import grad
+from toy import Simple2DGaussianMixture
+import matplotlib.pyplot as plt
 
 
+def leapfrog(theta, v, eps, L, gradU=None, U=None, pi=None, visited=None):
+    """
+    Leapfrog integration. At least one argument among gradU, U and pi must be given (preferably gradU)
+    :param theta: initial position
+    :param v: initial momentum
+    :param eps: step size
+    :param L: number of steps
+    :param gradU: gradient of the potential function U. Defaults to None.
+    :param U: potential function U. Defaults to None.
+    :param pi: target distribution up to a multiplicative constant (pi is prop. to exp(-U)). Defaults to None.
+    :param visited: list to add each visited node (we do nothing if it is None)
+    :return: tuple (theta,v) obtained with L steps of the integrator
+    """
+    # gradient of U
+    if gradU is None and U is None and pi is None:
+        raise ValueError("At least one among gradU, U and pi should be given")
+    if gradU is None:
+        U = lambda x: -np.log(pi(x)) if U is None else U
+        gradU = grad(U)
+    # Leapfrog
+    # Notice that this implementation may look like Euler Integration, but it is indeed Leapfrog
+    # (the lines just before and just after the loop make all the difference)
+    for _ in range(L):
+        v = v - eps / 2 * gradU(theta)
+        theta = theta + eps * v
+        v = v - eps / 2 * gradU(theta)
+        if visited is not None:
+            visited.append((theta, v))
+    return theta, v
 
-def leapfrog():
-    pass
-    return
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # test code
-    pass
+    toy = Simple2DGaussianMixture()
+    gradU = grad(toy.U)
+    theta0 = np.array([0,-0.5])
+    v0 = np.array([0,-1])
+    eps = 0.1
+    visited = []
+    _ = leapfrog(theta0, v0, eps, int(4*np.pi/eps), gradU, visited=visited)
+    lfpathx = [x[0][0] for x in visited]
+    lfpathy = [x[0][1] for x in visited]
+
+    grid_lim = 3
+    nb_points = 100
+    xplot = np.linspace(-grid_lim, grid_lim, nb_points)
+    yplot = np.linspace(-grid_lim, grid_lim, nb_points)
+    X, Y = np.meshgrid(xplot, yplot)
+    s = np.dstack((X, Y))
+    Z = np.array([[toy.pi(s[i,j]) for j in range(nb_points)] for i in range(nb_points)]).squeeze()
+
+
+    plt.figure()
+    plt.contourf(X, Y, Z, cmap="PuBu_r")
+    plt.plot(lfpathx, lfpathy, marker='o', markersize=2, color='k', markeredgecolor='orange')
+    plt.show()
---
+++ toy.py
@@ -0,0 +1,52 @@
+import autograd.numpy as np
+from autograd import grad
+
+class Simple2DGaussianMixture():
+    """
+    Example with mixture of two gaussians
+    HOW TO USE:
+    - initialize: toy = Simple2DGaussianMixture()
+    - use it: toy.pi(x), toy.piUpToConstant(x), ...
+    """
+
+    def __init__(self):
+        # centers
+        self.mu1 = np.array([-0.5,-1])
+        self.mu2 = np.array([1,1])
+        # covariance matrices
+        self.sigma1 = np.array([[1., 0.5],[0.5, 1.]])
+        self.sigma1inv = np.linalg.inv(self.sigma1)
+        self.sigma1det = np.linalg.det(self.sigma1)
+        self.sigma2 = np.array([[.5, 0.1],[0.1, .5]])
+        self.sigma2inv = np.linalg.inv(self.sigma2)
+        self.sigma2det = np.linalg.det(self.sigma2)
+        # weights for the gaussians
+        self.w1 = 0.6
+        self.w2 = 0.4
+
+    def mvnpdf1(self, theta):
+        vec = (theta-self.mu1).reshape(-1,1)
+        return np.exp(-0.5*vec.T@self.sigma1inv@vec)/(2*np.pi*np.sqrt(self.sigma1det))
+
+    def mvnpdf2(self, theta):
+        vec = (theta - self.mu2).reshape(-1, 1)
+        return np.exp(-0.5*vec.T@self.sigma2inv@vec)/(2*np.pi*np.sqrt(self.sigma2det))
+
+    def pi(self, theta):
+        # mixture of two gaussians
+        return self.w1 * self.mvnpdf1(theta) + self.w2 * self.mvnpdf2(theta)
+
+    def piUpToConstant(self, theta):
+        return 5 * self.pi(theta)
+
+    def U(self, theta):
+        return -np.log(self.pi(theta))
+
+    def UUpToConstant(self, theta):
+        return -np.log(self.piUpToConstant(theta))
+
+    def H(self, theta, v, M=np.eye(2)):
+        return self.U(theta) + v.T @ M @ v / 2
+
+    def HUpToConstant(self, theta, v, M=np.eye(2)):
+        return self.UUpToConstant(theta) + v.T @ M @ v / 2
