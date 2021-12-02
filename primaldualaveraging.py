bafcc216824be6ae2bbff856f256cc10d0e08c69
nam <ngtnams@gmail.com>
2021-12-02 02:28:14 +0100

    log to np.log

--- primaldualaveraging.py
+++ primaldualaveraging.py
@@ -16,13 +16,13 @@ def findReasonableEpsilon(theta, U):
     v = st.norm(0, 1).rvs(n)
     log_prob = -U(theta) - np.dot(v, v) / 2
     tilde_log_prob = -U(tilde_theta) - np.dot(tilde_v, tilde_v) / 2
-    condition = tilde_log_prob - log_prob + log(2)
+    condition = tilde_log_prob - log_prob + np.log(2)
     a = 2 * (condition >0) - 1
     while a * condition > 0:
         eps = 2**a * eps
         tilde_theta, tilde_v = leapfrog(theta=theta, v=v, eps=eps, L=1, U=U)
         tilde_log_prob = -U(tilde_theta) - np.dot(tilde_v, tilde_v) / 2
-        condition = tilde_log_prob - log_prob + log(2)
+        condition = tilde_log_prob - log_prob + np.log(2)
     return eps
 
 
