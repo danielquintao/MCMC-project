6a07f1e1849958e273621e6897a2e51a3031dd00
nam <ngtnams@gmail.com>
2021-12-02 02:24:43 +0100

    Function "find a good step size eps"

--- primaldualaveraging.py
+++ primaldualaveraging.py
@@ -1,12 +1,30 @@
+import autograd.numpy as np
+from leapfrog import leapfrog
+import scipy.stats as st
 
 
-def findReasonableEpsilon():
+def findReasonableEpsilon(theta, U):
     '''
-    Heuristics to the initial epsilon BEFORE tuning
-    :return:
+    Heuristics for choosing an initial value of epsilon BEFORE tuning
+    -- Remark as in leapflog file, we suppose at this version that M = Identity matrix---
+    :return: eps
     '''
-    pass
-    return
+    eps = 1
+    n = len(theta)
+    #mean, cov = np.zeros(n), numpy.eye(n)
+    #v = np.random.multivariate_normal(mean, cov)
+    v = st.norm(0, 1).rvs(n)
+    log_prob = -U(theta) - np.dot(v, v) / 2
+    tilde_log_prob = -U(tilde_theta) - np.dot(tilde_v, tilde_v) / 2
+    condition = tilde_log_prob - log_prob + log(2)
+    a = 2 * (condition >0) - 1
+    while a * condition > 0:
+        eps = 2**a * eps
+        tilde_theta, tilde_v = leapfrog(theta=theta, v=v, eps=eps, L=1, U=U)
+        tilde_log_prob = -U(tilde_theta) - np.dot(tilde_v, tilde_v) / 2
+        condition = tilde_log_prob - log_prob + log(2)
+    return eps
+
 
 def tuneEpsilon():
     pass
