3157b47e50b5ecb31e10a52e1ab1d5606075ef84
nam <ngtnams@gmail.com>
2021-12-03 18:05:50 +0100

    utils: acceptance proba function

---
+++ utils.py
@@ -0,0 +1,24 @@
+import autograd.numpy as np
+def log_accept_proba(theta, v, theta_new, v_new, U):
+    """
+    Compute log acceptance probability
+    """
+    H_old = U(theta) + 0.5 * np.sum(v * v)
+    H_new = U(theta_new) + 0.5 * np.sum(v_new * v_new)
+    log_accept = min(0, H_old - H_new)
+    return np.sum(log_accept)
+    #using np.sum only to remove the matrix parentheses to obtain a real number
+    # toy.U return a matrix of one element not a number
+
+
+if __name__=="__main__":
+    # test code
+    from toy import Simple2DGaussianMixture
+
+    toy = Simple2DGaussianMixture()
+    theta = np.array([1,2])
+    v = np.array([3, 4])
+    theta_new = np.array([5, 6])
+    v_new = np.array([7, 8])
+
+    print(log_accept_proba(theta, v, theta_new, v_new, toy.U))
