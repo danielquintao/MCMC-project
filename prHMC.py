a35349fe045e866ca5b7e2b251e7c4cbdec0781c
OumaimaMarbouh77 <76117954+OumaimaMarbouh77@users.noreply.github.com>
2021-12-06 12:47:49 +0100

    Update prHMC.py

--- prHMC.py
+++ prHMC.py
@@ -1,7 +1,18 @@
+from leapfrog import leapfrog
+from longestbatch import computeEmpiricalBatchDistribution
+from utils import H
+
+import matplotlib.pyplot as plt
+
+from toy import Simple2DGaussianMixture
+from autograd import grad
+import autograd.numpy as np
+import numpy
+import time
+import math
 
 
-
-def eHMC(theta_0, eps, emp_L, eta, N, M=None,U=None, pi=None, visited=None):
+def prHMC(theta_0, eps, emp_L, eta, N, M=None,U=None, pi=None, visited=None):
     '''
     :param theta_0: starting position
     :param eps: step size
@@ -15,8 +26,48 @@ def eHMC(theta_0, eps, emp_L, eta, N, M=None,U=None, pi=None, visited=None):
     :param visited: list to add each visited node
     :return: list of accepted positions
     '''
+    thetas = [theta_0.reshape(1, -1)]
+    momentums = []
 
-    return
+    if U is None and pi is None:
+        raise ValueError("U or pi must be given")
+    if U is None:
+        U = lambda x: -np.log(pi(x))
+    gradU = grad(U)
+
+    if M is None:
+        M = np.eye(len(theta_0))
+
+    v_0 = np.random.multivariate_normal(np.zeros(len(theta_0)), M)
+    w_=(theta_0, v_0)
+    sigma=1
+    i=1
+    l=1
+    Minv = np.linalg.inv(M)
+
+    for n in range(N):
+        L= math.ceil(numpy.random.choice(emp_L, size=1)[0]/3)
+        mu = np.random.uniform(0, 1)
+        if mu < eta:
+            v = np.random.multivariate_normal(np.zeros(len(theta_0)), M)
+            w_=
+            print(LFpath(thetas[-1], v, eps, L, M, gradU, U, pi, visited))
+            l=L+1
+            rho = np.exp(H(thetas[-1], v, U, Minv) - H(, , U, Minv))
+            event = np.random.uniform(0, 1)
+            if event <= rho:
+                theta=
+                v=
+                i=l
+                sigma=1
+
+            else:
+
+        else:
+
+
+    return thetas
+
 
 def LFpath(theta, v, eps, L, M=None, gradU=None, U=None, pi=None):
     """
@@ -40,9 +91,12 @@ def LFpath(theta, v, eps, L, M=None, gradU=None, U=None, pi=None):
     if M is None:
         M = np.eye(len(theta))
     invM = np.linalg.inv(M)
-    visited = [(theta, v)]
+    visited = []
     # LF
+    v_star= v- eps / 2 * gradU(theta)
     for _ in range(L):
-        # TODO
+        theta = theta + eps * (invM @ v.reshape(-1, 1)).flatten()
+        v = v_star - eps / 2 * gradU(theta)
+        v_star = v - eps / 2 * gradU(theta)
         visited.append((theta, v))
     return visited
